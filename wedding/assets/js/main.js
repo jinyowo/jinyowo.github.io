8e500563546355efc5249b454439ad693699c7ff
jinyowo <hjin421@gmail.com>
2018-04-25 16:22:31 +0900

    editorconfig 추가, 파일 indent 수정

--- wedding/assets/js/main.js
+++ wedding/assets/js/main.js
@@ -3,216 +3,234 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-(function($) {
+(function ($) {
 
-	skel.breakpoints({
-		xxlarge: '(max-width: 1920px)',
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 1000px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)',
-	});
+    skel.breakpoints({
+        xxlarge: '(max-width: 1920px)',
+        xlarge: '(max-width: 1680px)',
+        large: '(max-width: 1280px)',
+        medium: '(max-width: 1000px)',
+        small: '(max-width: 736px)',
+        xsmall: '(max-width: 480px)',
+    });
 
-	$(function() {
+    $(function () {
 
-		var	$window = $(window),
-			$body = $('body'),
-			$header = $('#header'),
-			$all = $body.add($header);
+        var $window = $(window),
+            $body = $('body'),
+            $header = $('#header'),
+            $all = $body.add($header);
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+        $window.on('load', function () {
+            window.setTimeout(function () {
+                $body.removeClass('is-loading');
+            }, 100);
+        });
 
-		// Touch mode.
-			skel.on('change', function() {
+        // Touch mode.
+        skel.on('change', function () {
 
-				if (skel.vars.mobile || skel.breakpoint('small').active)
-					$body.addClass('is-touch');
-				else
-					$body.removeClass('is-touch');
+            if (skel.vars.mobile || skel.breakpoint('small').active)
+                $body.addClass('is-touch');
+            else
+                $body.removeClass('is-touch');
 
-			});
+        });
 
 
-		// Fix: IE flexbox fix.
-			if (skel.vars.IEVersion <= 11
-			&&	skel.vars.IEVersion >= 10) {
+        // Fix: IE flexbox fix.
+        if (skel.vars.IEVersion <= 11
+            && skel.vars.IEVersion >= 10) {
 
-				var $main = $('.main.fullscreen'),
-					IEResizeTimeout;
+            var $main = $('.main.fullscreen'),
+                IEResizeTimeout;
 
-				$window
-					.on('resize.ie-flexbox-fix', function() {
+            $window
+                .on('resize.ie-flexbox-fix', function () {
 
-						clearTimeout(IEResizeTimeout);
+                    clearTimeout(IEResizeTimeout);
 
-						IEResizeTimeout = setTimeout(function() {
+                    IEResizeTimeout = setTimeout(function () {
 
-							var wh = $window.height();
+                        var wh = $window.height();
 
-							$main.each(function() {
+                        $main.each(function () {
 
-								var $this = $(this);
+                            var $this = $(this);
 
-								$this.css('height', '');
+                            $this.css('height', '');
 
-								if ($this.height() <= wh)
-									$this.css('height', (wh - 50) + 'px');
+                            if ($this.height() <= wh)
+                                $this.css('height', (wh - 50) + 'px');
 
-							});
+                        });
 
-						});
+                    });
 
-					})
-					.triggerHandler('resize.ie-flexbox-fix');
+                })
+                .triggerHandler('resize.ie-flexbox-fix');
 
-			}
+        }
 
-		// Prioritize "important" elements on small.
-			skel.on('+small -small', function() {
-				$.prioritize(
-					'.important\\28 small\\29',
-					skel.breakpoint('small').active
-				);
-			});
+        // Prioritize "important" elements on small.
+        skel.on('+small -small', function () {
+            $.prioritize(
+                '.important\\28 small\\29',
+                skel.breakpoint('small').active
+            );
+        });
 
-		// Gallery.
-			$window.on('load', function() {
+        // Gallery.
+        $window.on('load', function () {
 
-				var $gallery = $('.gallery');
+            var $gallery = $('.gallery');
 
-				$gallery.poptrox({
-					baseZIndex: 10001,
-					useBodyOverflow: false,
-					usePopupEasyClose: false,
-					overlayColor: '#1f2328',
-					overlayOpacity: 0.65,
-					usePopupDefaultStyling: false,
-					usePopupCaption: false,
-					popupLoaderText: '',
-					windowMargin: 50,
-					usePopupNav: true
-				});
+            $gallery.poptrox({
+                baseZIndex: 10001,
+                useBodyOverflow: false,
+                usePopupEasyClose: false,
+                overlayColor: '#1f2328',
+                overlayOpacity: 0.65,
+                usePopupDefaultStyling: false,
+                usePopupCaption: false,
+                popupLoaderText: '',
+                windowMargin: 50,
+                usePopupNav: true
+            });
 
-				// Hack: Adjust margins when 'small' activates.
-					skel
-						.on('-small', function() {
-							$gallery.each(function() {
-								$(this)[0]._poptrox.windowMargin = 50;
-							});
-						})
-						.on('+small', function() {
-							$gallery.each(function() {
-								$(this)[0]._poptrox.windowMargin = 5;
-							});
-						});
+            // Hack: Adjust margins when 'small' activates.
+            skel
+                .on('-small', function () {
+                    $gallery.each(function () {
+                        $(this)[0]._poptrox.windowMargin = 50;
+                    });
+                })
+                .on('+small', function () {
+                    $gallery.each(function () {
+                        $(this)[0]._poptrox.windowMargin = 5;
+                    });
+                });
 
-			});
+        });
 
-		// Section transitions.
-			if (skel.canUse('transition')) {
+        // Section transitions.
+        if (skel.canUse('transition')) {
 
-				var on = function() {
+            var on = function () {
 
-					// Galleries.
-						$('.gallery')
-							.scrollex({
-								top:		'30vh',
-								bottom:		'30vh',
-								delay:		0,
-								initialize:	function() { $(this).addClass('inactive'); },
-								terminate:	function() { $(this).removeClass('inactive'); },
-								enter:		function() { $(this).removeClass('inactive'); },
-								// leave:		function() { $(this).addClass('inactive'); }
-							});
+                // Galleries.
+                $('.gallery')
+                    .scrollex({
+                        top: '30vh',
+                        bottom: '30vh',
+                        delay: 0,
+                        initialize: function () {
+                            $(this).addClass('inactive');
+                        },
+                        terminate: function () {
+                            $(this).removeClass('inactive');
+                        },
+                        enter: function () {
+                            $(this).removeClass('inactive');
+                        },
+                        // leave:		function() { $(this).addClass('inactive'); }
+                    });
 
-					// Generic sections.
-						$('.main.style1')
-							.scrollex({
-								mode:		'middle',
-								delay:		0,
-								initialize:	function() { $(this).addClass('inactive'); },
-								terminate:	function() { $(this).removeClass('inactive'); },
-								enter:		function() { $(this).removeClass('inactive'); },
-								// leave:		function() { $(this).addClass('inactive'); }
-							});
+                // Generic sections.
+                $('.main.style1')
+                    .scrollex({
+                        mode: 'middle',
+                        delay: 0,
+                        initialize: function () {
+                            $(this).addClass('inactive');
+                        },
+                        terminate: function () {
+                            $(this).removeClass('inactive');
+                        },
+                        enter: function () {
+                            $(this).removeClass('inactive');
+                        },
+                        // leave:		function() { $(this).addClass('inactive'); }
+                    });
 
-						$('.main.style2')
-							.scrollex({
-								mode:		'middle',
-								delay:		 0,
-								initialize:	function() { $(this).addClass('inactive'); },
-								terminate:	function() { $(this).removeClass('inactive'); },
-								enter:		function() { $(this).removeClass('inactive'); },
-								// leave:		function() { $(this).addClass('inactive'); }
-							});
+                $('.main.style2')
+                    .scrollex({
+                        mode: 'middle',
+                        delay: 0,
+                        initialize: function () {
+                            $(this).addClass('inactive');
+                        },
+                        terminate: function () {
+                            $(this).removeClass('inactive');
+                        },
+                        enter: function () {
+                            $(this).removeClass('inactive');
+                        },
+                        // leave:		function() { $(this).addClass('inactive'); }
+                    });
 
-				};
+            };
 
-				var off = function() {
+            var off = function () {
 
-					// // Galleries.
-					// 	$('.gallery')
-					// 		.unscrollex();
-                    //
-					// // Generic sections.
-					// 	$('.main.style1')
-					// 		.unscrollex();
-                    //
-					// 	$('.main.style2')
-					// 		.unscrollex();
-				};
+                // // Galleries.
+                // 	$('.gallery')
+                // 		.unscrollex();
+                //
+                // // Generic sections.
+                // 	$('.main.style1')
+                // 		.unscrollex();
+                //
+                // 	$('.main.style2')
+                // 		.unscrollex();
+            };
 
-				skel.on('change', function() {
+            skel.on('change', function () {
 
-					if (skel.breakpoint('small').active)
-						(off)();
-					else
-						(on)();
+                if (skel.breakpoint('small').active)
+                    (off)();
+                else
+                    (on)();
 
-				});
+            });
 
-			}
+        }
 
-		// // Events.
-		// 	var resizeTimeout, resizeScrollTimeout;
+        // // Events.
+        // 	var resizeTimeout, resizeScrollTimeout;
         //
-		// 	$window
-		// 		.resize(function() {
+        // 	$window
+        // 		.resize(function() {
         //
-		// 			// Disable animations/transitions.
-		// 				$body.addClass('is-resizing');
+        // 			// Disable animations/transitions.
+        // 				$body.addClass('is-resizing');
         //
-		// 			window.clearTimeout(resizeTimeout);
+        // 			window.clearTimeout(resizeTimeout);
         //
-		// 			resizeTimeout = window.setTimeout(function() {
+        // 			resizeTimeout = window.setTimeout(function() {
         //
-		// 				// Update scrolly links.
-		// 					$('a[href^="#"]').scrolly({
-		// 						speed: 1500,
-		// 						offset: $header.outerHeight() - 1
-		// 					});
+        // 				// Update scrolly links.
+        // 					$('a[href^="#"]').scrolly({
+        // 						speed: 1500,
+        // 						offset: $header.outerHeight() - 1
+        // 					});
         //
-		// 				// Re-enable animations/transitions.
-		// 					window.setTimeout(function() {
-		// 						$body.removeClass('is-resizing');
-		// 						$window.trigger('scroll');
-		// 					}, 0);
+        // 				// Re-enable animations/transitions.
+        // 					window.setTimeout(function() {
+        // 						$body.removeClass('is-resizing');
+        // 						$window.trigger('scroll');
+        // 					}, 0);
         //
-		// 			}, 100);
+        // 			}, 100);
         //
-		// 		})
-		// 		.load(function() {
-		// 			$window.trigger('resize');
-		// 		});
+        // 		})
+        // 		.load(function() {
+        // 			$window.trigger('resize');
+        // 		});
 
-	});
+    });
 
 })(jQuery);
