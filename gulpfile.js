d0e05b32b65af3a2c6f99b9bc7d4974307a0f500
jinyowo <hjin421@gmail.com>
2018-05-03 18:31:33 +0900

    gulp release task 추가, UI 수정

--- gulpfile.js
+++ gulpfile.js
@@ -7,35 +7,76 @@ var minifyhtml = require('gulp-minify-html');
 var rename = require('gulp-rename');
 
 var paths = {
-    js: 'src/js',
-    scss: 'src/scss/*.scss',
-    html: 'src/index.html'
+    js: 'src/assets/js/*.js',
+    css: 'src/assets/css/*.css',
+    html: 'src/assets/index.html'
 };
 
-gulp.task('minify-js', function () {
-    return gulp.src([
-        paths.js + '/util.js',
-        paths.js + '/map.js',
-        paths.js + '/sns.js',
-        paths.js + '/main.js'
-    ])
+// gulp.task('minify-js', function () {
+//     return gulp.src([
+//         paths.js + '/util.js',
+//         paths.js + '/map.js',
+//         paths.js + '/sns.js',
+//         paths.js + '/main.js'
+//     ])
+//         .pipe(concat('script.min.js'))
+//         .pipe(uglify().on('error', gutil.log))
+//         .pipe(gulp.dest('assets/js'));
+// });
+//
+// gulp.task('minify-css', function () {
+//     return gulp.src(paths.scss)
+//         .pipe(minifyCSS())
+//         .pipe(rename('jinyowo-theme-min.scss'))
+//         .pipe(gulp.dest('_sass'));
+// });
+//
+// gulp.task('compress-html', function () {
+//     return gulp.src(paths.html)
+//         .pipe(minifyhtml())
+//         .pipe(rename('default.html'))
+//         .pipe(gulp.dest('_layouts'));
+// });
+
+gulp.task('release-js', function () {
+    return gulp.src(paths.js)
         .pipe(concat('script.min.js'))
         .pipe(uglify().on('error', gutil.log))
         .pipe(gulp.dest('assets/js'));
 });
 
-gulp.task('minify-css', function () {
-    return gulp.src(paths.scss)
+gulp.task('release-css', function () {
+    return gulp.src(paths.css)
         .pipe(minifyCSS())
-        .pipe(rename('jinyowo-theme-min.scss'))
-        .pipe(gulp.dest('_sass'));
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('assets/css'));
 });
 
-gulp.task('compress-html', function () {
-    return gulp.src(paths.html)
-        .pipe(minifyhtml())
-        .pipe(rename('default.html'))
-        .pipe(gulp.dest('_layouts'));
+gulp.task('copy-img', function () {
+    return gulp.src('src/assets/images/**/*.*')
+        .pipe(gulp.dest('assets/images'))
 });
 
-gulp.task('default', ['minify-js', 'minify-css', 'compress-html']);
+gulp.task('copy-font', function () {
+    return gulp.src('src/assets/fonts/*.*')
+        .pipe(gulp.dest('assets/fonts'))
+});
+
+gulp.task('copy-js-lib', function () {
+    return gulp.src('src/assets/js/lib/*.js')
+        .pipe(gulp.dest('assets/js/lib'))
+});
+
+gulp.task('copy-css', function () {
+    return gulp.src([
+        'src/assets/css/**/*.*',
+        '!src/assets/css/style.css'
+    ])
+        .pipe(gulp.dest('assets/css'))
+});
+
+
+// gulp.task('default', ['minify-js', 'minify-css', 'compress-html']);
+
+gulp.task('copy', ['copy-js-lib', 'copy-css', 'copy-img', 'copy-font']);
+gulp.task('release', ['release-js', 'release-css', 'copy']);
